184b00c9847b72604e5d28a6e427944bd0c7548a
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-05-20 17:50:43 -0600

    Complete Book class; tests pass

--- src/book.js
+++ src/book.js
@@ -1,11 +1,30 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+  return character
+}
+
+function saveReview(review, reviewList) {
+  if (reviewList.includes(review)) {
+    return reviewList
+  } else {
+    reviewList.push(review)
+  }
+}
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
