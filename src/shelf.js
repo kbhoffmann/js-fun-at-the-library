1a33c4b3fbfa2a5bcf3f2d486a42741310dc1551
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-05-21 11:37:26 -0600

    Complete shelf functions and tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,39 @@
+function  shelfBook (book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book)
+  }
+}
+
+function unshelfBook (bookName, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookName) {
+      shelf.splice(i, 1)
+    }
+  }
+  return shelf
+}
+
+function listTitles(shelf) {
+  var titlesArray = []
+  for (var i = 0; i < shelf.length; i++) {
+     titlesArray.push(shelf[i].title)
+  }
+  return titlesArray.join(', ') ;
+}
+
+function searchShelf (shelf, bookTitle) {
+  hasBook = false
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookTitle) {
+      hasBook = true
+    }
+  }
+  return hasBook
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
